dfc82f6a04106fba674cdfeee62d52684d91dfbe
Priyank Bhambu <pbhambu07@gmail.com>
2025-10-16 20:39:24 -0700

    Initial Commit

---
+++ Assets/PipeMoveScript.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+public class PipeMoveScript : MonoBehaviour
+{
+    public float moveSpeed = 2;
+    public float deadZone = -45;
+  
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < deadZone)
+        {
+            Destroy(gameObject);
+        }
+        transform.position = transform.position + (Vector3.left * moveSpeed) *Time.deltaTime;
+
+      
+    }
+}
